d27472bb3d7e92db25c823312454afdcfc1a0182
songjh11 <jhmelody91@gmail.com>
2022-09-20 16:17:43 +0900

    0920-4

---
+++ src/main/java/com/pet/home/sell/sellimpl/HotellingController.java
@@ -0,0 +1,10 @@
+package com.pet.home.sell.sellimpl;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/hotel/*")
+public class HotellingController {
+
+}
---
+++ src/main/java/com/pet/home/sell/sellimpl/TrainingController.java
@@ -0,0 +1,10 @@
+package com.pet.home.sell.sellimpl;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/training/*")
+public class TrainingController {
+
+}

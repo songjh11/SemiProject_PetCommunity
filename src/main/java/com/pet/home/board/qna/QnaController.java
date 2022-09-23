dbb3e6cd03cc465ed04e2c5ae57d0f613c18fe9d
hyeongminyoo <dbgudals2004@gmail.com>
2022-09-23 15:57:31 +0900

    QNA ~

--- src/main/java/com/pet/home/board/qna/QnaController.java
+++ src/main/java/com/pet/home/board/qna/QnaController.java
@@ -1,8 +1,67 @@
 package com.pet.home.board.qna;
 
+import java.util.List;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.fasterxml.jackson.annotation.JsonCreator.Mode;
+import com.pet.home.board.impl.BoardDTO;
+import com.pet.home.util.Pager;
 
 @Controller
+@RequestMapping(value = "/qna/*")
 public class QnaController {
+	
+	@Autowired
+	private QnaService qnaService;
+	
+	@ModelAttribute("board")
+	public String getBoard() {
+		return "qna";
+	}
+	
+	@RequestMapping("list")
+	public ModelAndView getList(Pager pager, HttpSession session) throws Exception{
+		ModelAndView mv = new ModelAndView();
+		List<BoardDTO> list = qnaService.getList(pager);
+		mv.addObject("list", list);
+		mv.addObject("pager", pager);
+		mv.setViewName("board/list");
+		
+		return mv;
+	}
+	
+	@RequestMapping("add")
+	public String setAdd() throws Exception{
+		return "board/add";
+	}
+	
+	@PostMapping("add")
+	public void setAdd(ModelAndView mv, BoardDTO boardDTO, HttpSession session, MultipartFile [] multipartFiles) throws Exception{
+		int result = qnaService.setAdd(boardDTO, multipartFiles, session.getServletContext());
+	}
+	
+	
+	@GetMapping("delete")
+	public void setDelete(ModelAndView mv, BoardDTO boardDTO) throws Exception{
+		int result = qnaService.setDelete(boardDTO);
+	}
+	
+	@GetMapping("detail")
+	public ModelAndView getDetail(ModelAndView mv, BoardDTO boardDTO) throws Exception{
+		boardDTO = qnaService.getDetail(boardDTO);
+		mv.addObject("dto", boardDTO);
+		mv.setViewName("board/detail");
+		return mv;
+	}
 	
 }
--- src/main/java/com/pet/home/board/qna/QnaService.java
+++ src/main/java/com/pet/home/board/qna/QnaService.java
@@ -4,52 +4,89 @@ import java.util.List;
 
 import javax.servlet.ServletContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.pet.home.board.impl.BoardDTO;
 import com.pet.home.board.impl.BoardFileDTO;
 import com.pet.home.board.impl.BoardService;
+import com.pet.home.board.notice.NoticeDAO;
+import com.pet.home.util.FileManager;
 import com.pet.home.util.Pager;
 
 @Service
 public class QnaService implements BoardService {
-
+	
+	@Autowired
+	private QnaDAO qnaDAO;
+	private FileManager fileManager;
+	
+	
 	@Override
 	public List<BoardDTO> getList(Pager pager) throws Exception {
 		// TODO Auto-generated method stub
-		return null;
+		pager.getRowNum();
+		pager.getNum(qnaDAO.getCount(pager));
+		
+		return qnaDAO.getList(pager) ;
 	}
 
 	@Override
 	public int setAdd(BoardDTO boardDTO, MultipartFile[] multipartFiles, ServletContext servletContext)
 			throws Exception {
 		// TODO Auto-generated method stub
-		return 0;
+		
+		int result = qnaDAO.setAdd(boardDTO);
+		String path = "resources/upload/qna";
+		for(MultipartFile multipartFile : multipartFiles) {
+			if(multipartFile.isEmpty()) {
+				continue;
+			}
+			String fileName = fileManager.saveFile(servletContext, path, multipartFile);
+			BoardFileDTO boardFileDTO = new BoardFileDTO();
+			boardFileDTO.setFileName(fileName);
+			boardFileDTO.setOriName(multipartFile.getOriginalFilename());
+			boardFileDTO.setNum(boardDTO.getNum());
+			qnaDAO.setFileAdd(boardFileDTO);
+		}
+		
+		
+		return result;
 	}
 
 	@Override
 	public BoardDTO getDetail(BoardDTO boardDTO) throws Exception {
 		// TODO Auto-generated method stub
-		return null;
+		return qnaDAO.getDetail(boardDTO);
 	}
 
 	@Override
 	public int setUpdate(BoardDTO boardDTO) throws Exception {
 		// TODO Auto-generated method stub
-		return 0;
+		return qnaDAO.setUpdate(boardDTO);
 	}
 
 	@Override
 	public int setDelete(BoardDTO boardDTO) throws Exception {
 		// TODO Auto-generated method stub
-		return 0;
+		return qnaDAO.setDelete(boardDTO);
 	}
 
 	@Override
 	public int setFileDelete(BoardFileDTO boardFileDTO, ServletContext servletContext) throws Exception {
 		// TODO Auto-generated method stub
-		return 0;
+		boardFileDTO = qnaDAO.getFileDetail(boardFileDTO);
+		
+		int result = qnaDAO.setFileDelete(boardFileDTO);
+		
+		String path = "resources/upload/qna";
+		
+		if(result > 0) {
+			fileManager.deleteFile(servletContext, boardFileDTO, path);
+		}
+		
+		return result;
 	}
 	
 	
